b8c10d2460939079ef0a4d1f3696f974b4e46e67
Chance Hudson <jchancehud@gmail.com>
2019-06-08 21:08:41 -0500

    Linting config

--- index.tsx
+++ index.tsx
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
 import React from 'react'
 import { Provider } from 'mobx-react'
 
