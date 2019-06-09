8f83af8c3c40a3e0e785e72985124ae9c6c5a779
Chance Hudson <jchancehud@gmail.com>
2019-06-08 20:44:48 -0500

    Add mobx

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react'
+import { Provider } from 'mobx-react'
 
-AppRegistry.registerComponent(appName, () => App);
+const stores = {}
+
+const _App = () => (
+  <Provider stores>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => _App)
