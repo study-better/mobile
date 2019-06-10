ab59b6d19a7ca1e0f7a3771322cec959a64f2e33
Chance Hudson <jchancehud@gmail.com>
2019-06-10 00:36:38 -0500

    Use header, add margin

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { View, Text } from 'react-native'
 import AuthStore from './stores/auth'
+import { NavigationScreenProps } from 'react-navigation'
 
 class Home extends React.Component<{
   auth: AuthStore
 }> {
+  static navigationOptions = ({ navigation }: NavigationScreenProps) => ({
+    title: 'Study Better',
+  })
+
   render() {
     return (
-      <View style={{}}>
-        <Text style={{}}>Welcome to Study Better!</Text>
+      <View style={{ margin: 8 }}>
+        <Text style={{}}>Welcome</Text>
         <Text>
-          You are{this.props.auth.authenticated ? ' ' : ' not'}authenticated
+          You are{this.props.auth.authenticated ? ' ' : ' not '}authenticated
         </Text>
       </View>
     )
